7bdf20d0936f5634b5c582bd9e52b068f6a651ef
chris <chris@Cristian>
2023-06-25 02:40:52 -0500

    CREACION DEL PROYECTO

---
+++ POO4P1/src/main/java/com/mycompany/poo4p1/POO4P1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo4p1;
+
+/**
+ *
+ * @author chris
+ */
+public class POO4P1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
